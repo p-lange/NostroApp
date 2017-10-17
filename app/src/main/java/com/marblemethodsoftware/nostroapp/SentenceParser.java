ef4da67a9852e956a5a27167dcf33baaae363b0a
Peter Lange <peter@marblemethod.io>
2017-10-17 15:25:40 -0700

    RAW sources updated

--- app/src/main/java/com/marblemethodsoftware/nostroapp/SentenceParser.java
+++ app/src/main/java/com/marblemethodsoftware/nostroapp/SentenceParser.java
@@ -5,13 +5,9 @@ import java.util.regex.Pattern;
 public class SentenceParser {
 
 
-
     public static String[] getStringArray(String sentence) {
 
-//        String pattern = "(\\s+\\|\\d+\\|\\w+\\|[^\\d\\s\\w])";
         String pattern = "\\s+";
-//        String pattern = "\\w+|\\s+";
-//        String pattern = "\\w+";
 
         return sentence.split(pattern);
 
